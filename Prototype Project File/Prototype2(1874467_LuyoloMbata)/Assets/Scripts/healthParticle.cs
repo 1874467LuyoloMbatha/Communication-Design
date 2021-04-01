7704c243ec3b0409702fe9cf5469a014e7ddb128
spovathekid <spovathekid@gmail.com>
2021-04-01 04:29:16 +0200

    Final Bild(part 2)
    This is the final build, the one I intend to submit. Previously I forgot to place the analysis essay in the Zip file. The file name is: Build2(LuyoloMbatha_1874467)

---
+++ Prototype Project File/Prototype2(1874467_LuyoloMbata)/Assets/Scripts/healthParticle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthParticle : MonoBehaviour
+{
+    public ParticleSystem hButtonParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void togglePSystem() 
+    {
+        if (hButtonParticle.isPlaying)
+        {
+            hButtonParticle.Stop();
+        }
+        else 
+        {
+            hButtonParticle.Play();
+
+
+        }
+
+
+    }
+}
